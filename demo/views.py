cc371ca8295987839bb73b55f5ac653f56bb857e
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-04-23 15:12:41 +0000

    Begin Templates

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 # Create your views here.
+class Another(View) :
+      book = Book.objects.get(id=1)
+      output=f'We have {book.title} book in db with a Id {book.id}<br>'
+      # for book in  books:
+      #       output += f'We have {book.title} book in db with a Id {book.id}<br>'
+      def get(self, request):
+            return HttpResponse(self.output)
+
+def first(request):
+      return HttpResponse("First Message Of Views")
